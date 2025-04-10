def8f48da4c19a8747cac7a485cf050778c25f93
Kostya <kostyakazmiruk@gmail.com>
2025-04-10 15:14:39 +0300

    Swagger init, class validator, class-transformer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import process from 'node:process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization,Accept',
+    credentials: true,
+  });
+
+  const options = new DocumentBuilder()
+    .setTitle('Github CRM')
+    .setDescription(
+      'This is a documentation about all possible api. They could be used with /api prefix',
+    )
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  const port: number = parseInt(process.env.SERVICE_PORT ?? '8000', 10);
+  await app.listen(port);
+  console.log(`Listening ${port}`);
 }
 bootstrap();
